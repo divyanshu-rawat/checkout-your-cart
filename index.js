3122c810bb33665617c26d319bf4a5c7507181c4
divyanshu <divyanshu.r46956@gmail.com>
2017-02-11 16:56:13 +0530

    debugging application !

--- index.js
+++ index.js
@@ -18,5 +18,15 @@ app.use('/api/v1', require('./api')(wagner));
 // file.
 app.use(express.static('front-end', { maxAge: 4 * 60 * 60 * 1000 /* 2hrs */ }));
 
-app.listen(5000);
-console.log('Listening on port 3000!');
+// app.listen(5000);
+// console.log('Listening on port 3000!');
+
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
